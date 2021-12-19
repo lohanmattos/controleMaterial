827ebb85c3ab3a3455c39b246178372eeb75ec15
Lohan Amendola <lohanamendola18@gmail.com>
2021-12-19 17:03:35 -0400

    app criado

---
+++ src/main/java/com/material/controleMaterial/ControleMaterialApplication.java
@@ -0,0 +1,13 @@
+package com.material.controleMaterial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleMaterialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleMaterialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/material/controleMaterial/ControleMaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.material.controleMaterial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleMaterialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
