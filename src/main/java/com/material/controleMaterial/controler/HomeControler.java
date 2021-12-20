521fa9768afac71177a57a950537952f1137a1c6
Lohan Amendola <lohanamendola18@gmail.com>
2021-12-19 20:40:34 -0400

    criado a pagina home

---
+++ src/main/java/com/material/controleMaterial/controler/HomeControler.java
@@ -0,0 +1,14 @@
+package com.material.controleMaterial.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeControler {
+	
+	@RequestMapping("/")
+	public String Home() {
+		return "home";
+	}
+
+}
