c36e8f688428a70636142b7fbcebfabe0b320a65
Lohan Amendola <lohanamendola18@gmail.com>
2021-12-20 09:18:22 -0400

    atualização do frontend

--- src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
+++ src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.material.controleMaterial.model.Produto;
 import com.material.controleMaterial.repositorio.ProdutoRepositorio;
 
 
@@ -20,5 +24,19 @@ public class ProdutoControle {
 		return "home";
 		
 	}
+	
+	@GetMapping("/novo")
+	public String novoProduto(Model model, Produto produto) {
+		model.addAttribute("produto", produto);
+		return "cadastrarProduto";
+	}
+	
+	//Deletar Produto
+	@RequestMapping(path = "/deletar/{id}", method = RequestMethod.GET)
+	public String excluirProduto(@PathVariable String id) {
+		produtoRepositorio.deleteById(id);
+		return "redirect:/";
+	}
+	
 
 }
