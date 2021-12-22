454f539973a4b6dc695eab5f6bfb2a615d4d2d8a
Lohan Amendola <lohanamendola18@gmail.com>
2021-12-22 10:23:40 -0400

    Deploy da aplicação

--- src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
+++ src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
@@ -1,8 +1,10 @@
 package com.material.controleMaterial.controler;
 
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -61,27 +63,9 @@ public class ProdutoControle {
 		return "editarProduto";
 	}
 		
-	@GetMapping(path ="/listar")
-	public String findId(@RequestParam String numeroPatrimonial, String nomeclatura, Model model) {
+
+
 		
-		Optional<Produto> produto = produtoRepositorio.findById(numeroPatrimonial);
-		
-		if (produto.isPresent()) {
-			
-			List<Produto> produtoListado =  produto.stream().
-					filter(p -> p.getNumeroPatrimonial() == numeroPatrimonial || p.getNomeclatura() == nomeclatura)
-					.toList();
-			model.addAttribute("listarProduto", produtoListado);
-			
-			return "home"; 
-		}else {
-			
-			return "redirect:/produtos" ;
-		}
-		
-	}
-		
-	
 	//Deletar Produto
 	@RequestMapping(path = "/deletar/{id}", method = RequestMethod.GET)
 	public String excluirProduto(@PathVariable String id) {
