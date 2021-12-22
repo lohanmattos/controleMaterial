aab332478b20604d65245f04f59ac555c3b6fc25
Lohan Amendola <lohanamendola18@gmail.com>
2021-12-22 19:47:59 -0400

    app

--- src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
+++ src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
@@ -73,15 +73,25 @@ public class ProdutoControle {
 	
 	
 	@GetMapping(path ="/listar")
-	public String findId(@RequestParam String numeroPatrimonial, Model model) {
+	public String findId(@RequestParam String numeroPatrimonial, String nomeclatura, Model model) {
 		
-		 Optional<Produto> produto1 = produtoRepositorio.findById(numeroPatrimonial);
+		Optional<Produto> produto = produtoRepositorio.findById(numeroPatrimonial);
+		
+		if (produto.isPresent()) {
 			
-			model.addAttribute("listarProduto", produto1);
+			List<Produto> produtoListado =  produto.stream().
+					filter(p -> p.getNumeroPatrimonial() == numeroPatrimonial || p.getNomeclatura() == nomeclatura)
+					.toList();
+			model.addAttribute("listarProduto", produtoListado);
+			
+			return "home"; 
+		}else {
 			
 			return "redirect:/produtos" ;
-			
+		}
+		
 	}
+
 		
 
 }
