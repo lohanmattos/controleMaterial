4bf1d9115e51f82e08eaa78ef81971e8de94be29
Lohan Amendola <lohanamendola18@gmail.com>
2021-12-22 19:16:25 -0400

    app

--- src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
+++ src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
@@ -76,8 +76,8 @@ public class ProdutoControle {
 		
 		Optional<Produto> produto1 = produtoRepositorio.findById(numeroPatrimonial);
 			
-			List<Produto> produtoListado =  produto1.stream().
-					filter(p -> p.getNumeroPatrimonial() == numeroPatrimonial || p.getNomeclatura() == nomeclatura)
+			List<Produto> produtoListado =  produto1.stream()
+					.filter(p -> p.getNumeroPatrimonial() == numeroPatrimonial || p.getNomeclatura() == nomeclatura)
 					.toList();
 			model.addAttribute("listarProduto", produtoListado);
 			
