b104def0f456082c604d76cb1a086416fb3aa32e
Lohan Amendola <lohanamendola18@gmail.com>
2021-12-23 08:29:49 -0400

    app

--- src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
+++ src/main/java/com/material/controleMaterial/controler/ProdutoControle.java
@@ -98,25 +98,7 @@ public class ProdutoControle {
 	}
 	
 	
-	@GetMapping(path ="/listar")
-	public String findId(@RequestParam String numeroPatrimonial, String nomeclatura, Model model) {
-		
-		Optional<Produto> produto = produtoRepositorio.findById(numeroPatrimonial);
-		
-		if (produto.isPresent()) {
-			
-			List<Produto> produtoListado =  produto.stream().
-					filter(p -> p.getNumeroPatrimonial() == numeroPatrimonial || p.getNomeclatura() == nomeclatura)
-					.toList();
-			model.addAttribute("listarProduto", produtoListado);
-			
-			return "home"; 
-		}else {
-			
-			return "redirect:/produtos" ;
-		}
-		
-	}
+
 	
 	
 	
